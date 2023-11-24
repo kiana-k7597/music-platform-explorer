1e8f6bfc34096f5328cefbefa317b22a4b4294af
kiana-k7597 <kiana.k7597@gmail.com>
2023-11-24 11:24:30 +0000

    Move API call to client class

---
+++ src/main/java/com/example/spotifyexplorer/client/SpotifyAPIClient.java
@@ -0,0 +1,31 @@
+package com.example.spotifyexplorer.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SpotifyAPIClient {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @Value("${spotify.bearer-token}")
+    private String bearerToken;
+
+    public String getNewAlbumRecommendations() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(bearerToken);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = this.restTemplate.exchange(
+                "https://api.spotify.com/v1/browse/new-releases",
+                HttpMethod.GET,
+                entity,
+                String.class);
+
+        return response.getBody();
+    }
+}
--- src/main/java/com/example/spotifyexplorer/service/NewAlbumService.java
+++ src/main/java/com/example/spotifyexplorer/service/NewAlbumService.java
@@ -1,46 +1,20 @@
 package com.example.spotifyexplorer.service;
 
+import com.example.spotifyexplorer.client.SpotifyAPIClient;
 import com.example.spotifyexplorer.models.Album;
 import com.example.spotifyexplorer.transformer.AlbumJsonTransformer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class NewAlbumService {
-    public NewAlbumService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    private final RestTemplate restTemplate;
-
-    @Value("${spotify.bearer-token}")
-    private String bearerToken;
-
-    public String getNewAlbumRecommendations() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(bearerToken);
-
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                "https://api.spotify.com/v1/browse/new-releases",
-                HttpMethod.GET,
-                entity,
-                String.class);
-
-        return response.getBody();
-    }
 
     public String getStringOutput() throws Exception {
 
-        String json = getNewAlbumRecommendations();
+        String response = new SpotifyAPIClient().getNewAlbumRecommendations();
 
-        List<Album> albumAndArtists = new AlbumJsonTransformer().parseAlbums(json);
+        List<Album> albumAndArtists = new AlbumJsonTransformer().parseAlbums(response);
 
         return "Here is new album recommendation: " + albumAndArtists.get(0).getName() +
                 " - " + albumAndArtists.get(0).getArtists().get(0).getName();
